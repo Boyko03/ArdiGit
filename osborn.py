fc7d4f04c0201ad53f0dc364be6f1b0e604704cf
Boyko03 <boyko100@abv.bg>
2018-03-16 21:25:43 +0200

    Update osborn.py

--- osborn.py
+++ osborn.py
@@ -1,9 +1,128 @@
-print("Osborne 1\n \
-The Osborne 1 was the first \n commercially successful \n portable microcomputer, \n released on April 3, \n 1981, by Osborne Computer \n Corporation. It weighed \n 10.7 kg (24.5 lb), \n cost US$1,795, and ran the \n CP/M 2.2 operating system. \n Powered directly from a mains \n socket as it had no on-board \n battery, it was still classed \n as a portable device since it \n could be hand carried when \n packed. \n \
-The Osborne 1 was \n developed by Adam \n Osborne and designed by Lee \n Felsenstein. It was first \n announced in early 1981. Osborne, \n an author of computer books, \n decided he wanted to break the \n price of computers. \n \
-The Osborne's design was \n based largely on the Xerox \n NoteTaker, a prototype \n developed at Xerox PARC in \n 1976 by Alan Kay. The \n computer was designed to be \n portable, with a rugged ABS \n plastic case that closed up, \n and a handle. The Osborne 1 \n was about the size and weight \n of a sewing machine and was \n advertised as the only \n computer that would fit \n underneath an airline seat. It \n is now classified as a \n 'luggable' computer when \n compared to later laptop \n designs such as the \n Epson HX-20. \n \
-Despite its unattractive design \n and heavy weight the \n computer amazed \n observers. BYTE wrote, ' it \n will cost $1795, and  it's \n portable!' ($1795 is the \n equivalent of $4832 today.) \n The bundled word processing, \n spreadsheet, and other \n software alone was worth \n $1,500; as InfoWorld stated in \n an April 1981 front-page \n article on the new computer \n after listing the included \n software, 'In case you think \n the price printed above was \n a mistake, we'll repeat it: \n $1795.' \n  \
-Osborne claimed that the \n new computer had a 'significant \n price/performance advantage' \n but emphasized the price, \n stating that its performance \n was 'merely adequate': 'It is \n not the fastest microcomputer, \n it doesn't have huge amounts \n of disk storage space, and it is \n not especially \n expandable.' Beyond \n the price, advertisements \n emphasized the computer's \n portability and bundled \n software. In the first eight \n months after April 1981, \n when the Osborne 1 was \n announced, the company sold \n 11,000 units. Sales at their \n peak reached 10,000 units \n per month. \n \
-Its principal deficiencies \n were a tiny 5-inch (13 cm) display \n screen and use of single sided, single density floppy disk drives which could not contain sufficient data for practical business applications, and considerable unit weight. A single-density disk controller was used to keep costs down. As a result, the Osborne's floppy disks held a mere 90k. \
-Later Osborne models switched to double-density controllers and an upgrade was offered to owners of the original model Osborne 1. \
-In September 1981, Osborne Computer Company had its first US$1 million sales month. Sales of the Osborne 1 were hurt by the company's premature announcement of superior successor machines such as the Osborne Executive, a phenomenon later called the Osborne effect.")
+print("Osborne 1 \n \
+The Osborne 1 was the first \n \
+commercially successful \n \
+portable microcomputer, \n \
+released on April3, 1981, by \n \
+Osborne Computer Corporation. \n \
+It weighed 10.7 kg \n \
+(24.5 lb), cost US$1,795, \n \
+and ran the CP/M 2.2 \n \
+operating system. Powered \n \
+directly from a mains socket \n \
+as it had no on-board \n \
+battery, it was still \n \
+classed as a portable \n \
+device since it could be \n \
+hand carried when packed. \n \
+\n \
+The Osborne 1 was developed \n \
+by Adam Osborne and \n \
+designed by Lee Felsenstein. \n \
+It was first announced in \n \
+early 1981. Osborne, \n \
+an author of computer books, \n \
+decided he wanted to break \n \
+the price of computers. \n \
+\n \
+The Osborne's design was \n \
+based largely on the Xerox \n \
+NoteTaker, a prototype \n \
+developed at Xerox PARC \n \
+in 1976 by Alan Kay. \n \
+The computer was designed \n \
+to be portable, with a \n \
+rugged ABS plastic case \n \
+that closed up, and a \n \
+handle. The Osborne 1 \n \
+was about the size and \n \
+weight of a sewing \n \
+machine and was advertised \n \
+as the only computer that \n \
+would fit underneath an \n \
+airline seat. It is now \n \
+classified as a 'luggable' \n \
+computer when compared \n \
+to later laptop designs \n \
+such as the \n \
+ \n \
+Epson HX-20. \n \
+Despite its unattractive \n \
+design and heavy weight \n \
+the computer amazed \n \
+observers. BYTE wrote, \n \
+' it will cost $1795, \n \
+and  it's portable!' \n \
+($1795 is the equivalent \n \
+of $4832 today.) The \n \
+bundled word processing, \n \
+spreadsheet, and other \n \
+software alone was worth \n \
+$1,500; as InfoWorld \n \
+stated in an April 1981 \n \
+front-page article on the \n \
+new computer after \n \
+listing the included \n \
+software, 'In case you \n \
+think the price printed \n \
+above was a mistake, \n \
+we'll repeat it: $1795.' \n \
+\n \
+Osborne claimed that \n \
+the new computer had a \n \
+'significant \n \
+price/performance \n \
+advantage' but emphasized \n \
+the price, stating that \n \
+its performance was \n \
+'merely adequate': \n \
+'It is not the fastest \n \
+microcomputer, it \n \
+doesn't have huge \n \
+amounts of disk storage \n \
+space, and it is not \n \
+especially expandable.' \n \
+Beyond the price, \n \
+advertisements emphasized \n \
+the computer's portability \n \
+and bundled software. In \n \
+the first eight months \n \
+after April 1981, when \n \
+the Osborne 1 was \n \
+announced, the company \n \
+sold 11,000 units. \n \
+Sales at their peak reached \n \
+10,000 units per month.  \n \
+\n \
+Its principal deficiencies \n \
+were a tiny 5-inch (13 cm) \n \
+display screen and use \n \
+of single sided, single \n \
+density floppy disk drives \n \
+which could not contain \n \
+sufficient data for \n \
+practical business \n \
+applications, and \n \
+considerable unit weight. \n \
+A single-density disk \n \
+controller was used to \n \
+keep costs down. As a \n \
+result, the Osborne's \n \
+floppy disks held a mere 90k. \n \
+\n \
+Later Osborne models \n \
+switched to double-density \n \
+controllers and an upgrade \n \
+was offered to owners of the \n \
+original model Osborne 1. \n \
+In September 1981, Osborne \n \
+Computer Company had its \n \
+first US$1 million sales \n \
+month. Sales of the \n \
+Osborne 1 were hurt by \n \
+the company's premature \n \
+announcement of superior \n \
+successor machines such as \n \
+the Osborne Executive, \n \
+a phenomenon later \n \
+called the Osborne effect. \
+")
