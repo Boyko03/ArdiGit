1f4b7fe237ad4d9bfc3febbb2752f51b708868d1
Boyko03 <boyko100@abv.bg>
2018-03-17 12:03:46 +0200

    Update notes.py

--- notes.py
+++ notes.py
@@ -1,69 +1,71 @@
-import sys
-import time
-import json
-import os
+def run():
+    import sys
+    import time
+    import json
+    import os
 
-array_notes = []
+    array_notes = []
 
-if os.stat("file.json").st_size != 0:
-    yesNo = input("You have already made some notes. Do you want to continue writting? Yes/No: ")
-    if yesNo == "yes":
-        with open('file.json') as file:
-            array_notes = json.loads(file.read())
+    if os.stat("file.json").st_size != 0:
+        yesNo = input("You have already made some notes. Do you want to continue writting? Yes/No: \n\
+(WARNING! If you type something != from 'y' or 'yes' you'll start new notes.): \n")
+        if yesNo == "yes" or yesNo == 'y':
+            with open('file.json') as file:
+                array_notes = json.loads(file.read())
 
-def array_format(array):
-    for i in range(len(array)):
-        format_string(array[i])
+    def array_format(array):
+        for i in range(len(array)):
+            format_string(array[i])
 
-def format_string(string):
-    a1 = str(string)
-    a1 = a1.strip()
-    a1 = a1.replace("[", "")
-    a1 = a1.replace("]", "")
-    a1 = a1.replace("'", "")
-    a1 = a1.replace("\t", "")
-    print(a1)
-    
-def search(text, array):
-    text = input("Search for: ")
-    count = 0
-    for i in array:
-        if text in i:
-            format_string(i)
-            count += 1
-    if count == 0:
-        print("The object was not found. ")
+    def format_string(string):
+        a1 = str(string)
+        a1 = a1.strip()
+        a1 = a1.replace("[", "")
+        a1 = a1.replace("]", "")
+        a1 = a1.replace("'", "")
+        a1 = a1.replace("\t", "")
+        print(a1)
+        
+    def search(text, array):
+        text = input("Search for: ")
+        count = 0
+        for i in array:
+            if text in i:
+                format_string(i)
+                count += 1
+        if count == 0:
+            print("The object was not found. ")
 
 
-print("\n Hello! This is your note sheet. You can add your notes here. \n If you type 'q' or 'quit' you will exit the program \n \
-and if you type 's' or 'search' you can find your notes by a keyword. \n")   
+    print("\n Hello! This is your note sheet. You can add your notes here. \n If you type 'q' or 'quit' you will exit the program \n \
+    and if you type 's' or 'search' you can find your notes by a keyword. \n")   
 
-text = input()
-if text == "quit" or text == "q":
-    array_format(array_notes)
-    time.sleep(2)
-    sys.exit()
-elif text == "search" or text == "s":
-    search(text, array_notes)
-else:
-    array_notes.append(text)
-
-while text != "quit" or text != "q":
     text = input()
     if text == "quit" or text == "q":
-        break
+        array_format(array_notes)
+        time.sleep(2)
+        return
     elif text == "search" or text == "s":
         search(text, array_notes)
     else:
-        array_notes.append(text)   
+        array_notes.append(text)
 
-array_format(array_notes)
+    while text != "quit" or text != "q":
+        text = input()
+        if text == "quit" or text == "q":
+            break
+        elif text == "search" or text == "s":
+            search(text, array_notes)
+        else:
+            array_notes.append(text)   
 
-with open('file.json', 'w') as file:
-	file.write(json.dumps(array_notes))
-	
-if len(array_notes) < 5:
-    time.sleep(len(array_notes))
-else:
-    time.sleep(3)
-sys.exit()
+    array_format(array_notes)
+
+    with open('file.json', 'w') as file:
+            file.write(json.dumps(array_notes))
+            
+    if len(array_notes) < 5:
+        time.sleep(len(array_notes))
+    else:
+        time.sleep(3)
+    return
