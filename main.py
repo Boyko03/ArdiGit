4928561e7ab0b6ea8687edafa51b5700e0ebda3c
Boyko03 <boyko100@abv.bg>
2018-03-17 12:04:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import notes
+import tuesHistory
+import osb
+import sys
+import time
+
 def write():
     print("Press '1' to view Osborne 1's history. \n\
 Press '2' to view BASIC's history. \n\
 Press '3' to view TUES's history. \n\
 Press '4' to view Game Boy's history. \n\
-Press '5' to type your notes. \n")
+Press '5' to type your notes. \n\
+If you want to exit the system\n\
+just type 'q' or 'quit'")
 
 
 def again(i, a):
@@ -12,22 +20,26 @@ def again(i, a):
         i = input("Error! Please, type again. \n")
     return i
 
-def run():    
+def runMain():    
     a = ['1', '2', '3', '4', '5']
     i = input()
+    if i == 'q' or i == 'quit':
+        print("You'll leave the system in 3 seconds.")
+        time.sleep(3)
+        sys.exit()
     if i not in a:
         i = again(i, a)
     else:
         if i == '1':
-            a = open("Osborne1Article.txt", 'r')
-            print(a.read(), '\n')
+            osb.run()
+            print()
             write()
         elif i == '2':
             a = open("Basic.txt", 'r')
             print(a.read(), '\n')
             write()
         elif i == '3':
-            import tuesHistory
+            tuesHistory.run()
             print()
             write()
         elif i == '4':
@@ -35,9 +47,10 @@ def run():
             print(a.read(), '\n')
             write()
         else:
-            import notes
+            notes.run()
+            print()
             write()
 
 write()
 while True:
-    run()
+    runMain()
